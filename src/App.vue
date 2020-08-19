db5e8abbc341cebd54b341ff901a8eb6b3f3c6cd
superlittlestar <superlittlestar@outlook.com>
2020-08-20 04:21:19 +0800

    【新建】vue项目工程 【编写功能】copy-wechat 用户列表选择用户，聊天框显示已选用户

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <div class="content">
+      <chatcom :curUser="curUser"></chatcom>
+      <userlistcom @selectUser="toggleUser" :userlist="userlist"></userlistcom>
+    </div>
+  </div>
+</template>
+<script>
+import chatcom from "./components/chatcom.vue";
+import userlistcom from "./components/userlistcom.vue";
+export default {
+  name: "App",
+  components: {
+    chatcom,
+    userlistcom
+  },
+
+  data() {
+    return {
+      msg: "Copy-Wechat",
+      userlist: [
+        {
+          username: "小明",
+          headerimg: require("./assets/image/1.jpg")
+        },
+        {
+          username: "小黄",
+          headerimg: require("./assets/image/2.jpg")
+        },
+        {
+          username: "小东",
+          headerimg: require("./assets/image/3.jpg")
+        },
+        {
+          username: "小星",
+          headerimg: require("./assets/image/4.jpg")
+        }
+      ],
+      curUser: {
+        username: "",
+        headerimg: ""
+      }
+    };
+  },
+  methods: {
+    toggleUser: function(index) {
+      this.curUser = this.userlist[index];
+    }
+  }
+};
+</script>
+<style scoped>
+.content {
+  display: flex;
+  width: 800px;
+  height: 700px;
+  margin: 50px auto;
+}
+</style>
---
+++ src/components/chatcom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="chat">
+    <h1 class="user">{{ curUser.username }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  props: ["curUser"]
+};
+</script>
+<style scoped>
+.chat {
+  width: 500px;
+  height: 700px;
+  border: 2px solid #cccccc;
+}
+</style>
