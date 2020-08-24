2888541d50c24c358929700db006cce2c86762a6
super-little-star <superlittlestar114@email.com>
2020-08-25 04:26:38 +0800

    【添加】mssql 链接到sqlserver 【完成】用户选择列表组件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <selectUser
+      v-if="$root.currentUser == null"
+      :userlist="userlist"
+    ></selectUser>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import selectUser from "./components/SelectUser";
+import axios from "axios";
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
+    selectUser
+  },
+  data() {
+    return {
+      userlist: []
+    };
+  },
+  beforeMount() {
+    var myself = this;
+    axios.get("http://localhost:3000/api/userlist").then(function(res) {
+      console.log(res);
+      myself.userlist = res.data.recordset;
+    });
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/SelectUser.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="selectUser">
+    <h1>选择你聊天的角色</h1>
+    <div class="list">
+      <div
+        class="item"
+        @click="selectEvent(item)"
+        v-for="(item, index) in userlist"
+        :key="index"
+      >
+        <img :src="item.headerImg" />
+        <span>{{ item.userName }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["userlist"],
+  methods: {
+    selectEvent: function(sel) {
+      this.$root.currentUser = sel;
+      localStorage.currentUser = JSON.stringify(sel);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.selectUser h1 {
+  font-size: 20px;
+  text-align: center;
+  padding: 20px 0;
+}
+.selectUser {
+  width: 100vw;
+  height: 100vh;
+  background: skyblue;
+}
+.list {
+  display: flex;
+  flex-wrap: wrap;
+}
+.item {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  height: 130px;
+  margin: 15px 0;
+}
+.item img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  box-shadow: 5px 0px 10px rgb(73, 73, 73);
+}
+</style>
